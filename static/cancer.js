e7e50500d1e6ae929e8130645dd3e7d2e230e298
Peter Chinman <peter.chinman@gmail.com>
2024-12-16 21:59:16 -0500

    mode option slider

--- static/cancer.js
+++ static/cancer.js
@@ -7,4 +7,27 @@ function silentCancer() {
    // console.log(cancer);
 }
 
+function cancerInterval(chance, interval){
+   return setInterval(() => {
+      // Generate a random number between 0 and 99
+      const randomChance = Math.floor(Math.random() * 100);
+
+      if (randomChance < chance) {
+         silentCancer();
+      }
+   }, interval);
+}
+
+document.addEventListener('click', (event) => {
+   if (event.target.closest('.silent-letter-container')) {
+      let $container = event.target.closest('.silent-letter-container');
+      const children = Array.from($container.children);
+      for (let i = 0; i < children.length; ++i) {
+         if (i > 0) {
+            $container.removeChild(children[i])
+         }
+      }
+   }
+})
+
 
